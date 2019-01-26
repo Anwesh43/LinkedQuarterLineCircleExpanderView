d659e3a6fd89dbc4931ee97b8687bfd68edea7e9
Anwesh43 <anweshthecool0@gmail.com>
2019-01-26 12:38:43 +0530

    adding delay as parameter

--- qcleview/src/main/java/com/anwesh/uiprojects/qcleview/QCLEView.kt
+++ qcleview/src/main/java/com/anwesh/uiprojects/qcleview/QCLEView.kt
@@ -22,6 +22,7 @@ val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#01579B")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val offset : Float = 10f
+val delay : Long = 10
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -104,7 +105,7 @@ class QCLEView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
