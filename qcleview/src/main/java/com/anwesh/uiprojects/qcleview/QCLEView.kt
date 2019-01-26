cfd824832173cdf34c945182dc8e325d315ab3d5
Anwesh43 <anweshthecool0@gmail.com>
2019-01-26 11:19:12 +0530

    adding libraries and defining nodes

--- qcleview/src/main/java/com/anwesh/uiprojects/qcleview/QCLEView.kt
+++ qcleview/src/main/java/com/anwesh/uiprojects/qcleview/QCLEView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.qcleview
 /**
  * Created by anweshmishra on 26/01/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val circles : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
